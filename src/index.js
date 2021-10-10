b61448088bf4c36222e228e25a333355b5e93514
ivanlog-git <52100245+ivanlog-git@users.noreply.github.com>
2021-10-10 12:54:58 +0300

    Cookies+mongodb

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const MongoClient = require("mongodb").MongoClient;
+
 
 const port = 5000;
 const app = express();
 const jsonParser = express.json();
 
+let dbClient;
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
 
+mongoClient.connect(function(err, client){      
+ 
+    if(err) return console.log(err);
+      
+    const db = client.db("ifcase_group_project");
+    app.locals.sessions = db.collection("sessions");    
+    app.listen(port, ()=>{console.log("Server listen at "+port.toString())});   
+});
 
-app.use((req, resp, next)=>{
-    console.log("Middleware");        
-    next();
-    console.log("Middleware 2");    
+function generateSid()
+{
+    let sid="s";
+    for(let i=0;i<64;i++)
+        sid+=(Math.trunc(Math.random()*17)).toString(16);
+    return sid;
+}
+
+app.use(cookieParser());
+app.use(async (req, resp, next)=>{
+    let sid = req.cookies.sid;
+    if (!sid) 
+    {
+        sid = generateSid();
+        resp.cookie("sid", sid);        
+    }
+    let session = await req.app.locals.sessions.findOne({sid:sid});
+    if (!session) 
+    {
+        session={sid:sid, counter:0};
+        req.app.locals.sessions.insertOne(session);
+    }
+    req.session = session;
+    console.log(JSON.stringify(session));
+    next();    
 })
 
 
@@ -19,27 +53,28 @@ app.get("/",(req, resp)=>{
 })
 
 
-let counter=10;
-
-app.get("/counter",(req, resp)=>{    
-    counter++;    
-    resp.send(JSON.stringify({counter}));
+app.get("/counter",(req, resp)=>{     
+    req.session.counter++;
+    resp.send(JSON.stringify({counter:req.session.counter}));
     console.log("Counter");    
 })
 
 
 app.get("/loadstate",(req, resp)=>{   
-    resp.send(JSON.stringify({counter}));    
+    resp.send(JSON.stringify({counter:req.session.counter}));    
 })
 
 
-app.post("/savestate", jsonParser, (req, resp)=>{
+app.post("/savestate", jsonParser, async (req, resp)=>{
     try
     {        
         let reqObj = req.body;                
         if (!reqObj) throw new Error("Wrong format");
         if (!reqObj.counter) throw new Error("Wrong data format");
-        counter = parseInt(reqObj.counter.toString());
+        let newCounter=parseInt(reqObj.counter.toString());
+
+        let newSession = await req.app.locals.sessions.findOneAndUpdate({sid:req.session.sid},{$set:{counter:newCounter}});        
+        req.session=newSession;
         resp.send(JSON.stringify({error:0}));
     } catch (error)
     {
@@ -49,7 +84,7 @@ app.post("/savestate", jsonParser, (req, resp)=>{
 
 
 
-app.listen(port, ()=>{console.log("Server listen at "+port.toString())});
+
 
 
 
